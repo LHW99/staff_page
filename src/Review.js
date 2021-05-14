a4fac4156395119958188215267b283e11ad9300
haiduk <hwlee91@gmail.com>
2021-05-14 01:23:46 -0400

    build ready

---
+++ src/Review.js
@@ -0,0 +1,50 @@
+import React,{useState}from 'react'
+import data from './data'
+
+const Review = () => {
+  const [reviews,setReviews] = useState(data)
+  const [showReview,setShowReview] = useState(1)
+
+  const forwardReview = () => {
+    if(showReview === data.length){
+      return setShowReview(1);
+    }
+    return setShowReview(showReview + 1);
+  };
+
+  const backReview = () => {
+    if(showReview === 1){
+      return setShowReview(data.length);
+    }
+    return setShowReview(showReview - 1);
+  };
+
+  const randomReview = () => {
+    let ranRev = Math.ceil(Math.random()*4)
+    return setShowReview(ranRev)
+  };
+
+  return (
+    <article className="review">
+      {reviews.map((review)=>{
+        const {id, name, job, image, text} = review;
+        if(id === showReview){
+        return (
+          <>
+            <div className="img-container"> 
+              <img src={image} alt={name} className="person-img"/>
+            </div>
+            <h2>{name}</h2>
+            <h3>{job}</h3>
+            <h4>{text}</h4>
+          </>
+        )}
+      })}
+      <button className="btn" onClick={()=>backReview()} className="prev-btn"> Previous </button>
+      <button className="btn" onClick={()=>randomReview()} className="random-btn">Surprise Me!</button>
+      <button className="btn" onClick={()=>forwardReview()} className="next-btn"> Next </button>
+    </article>
+  )
+};
+
+export default Review;
